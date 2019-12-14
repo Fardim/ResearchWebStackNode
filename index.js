181c76ff1a9a159e0d81a1474ba16ac0beae0b3f
FARDIM KAISER <fardim_drmc_9094@yahoo.com>
2019-12-14 20:36:56 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var soap = require("soap");
+const express = require("express");
+const app = express();
+const getTests = require("./routes/getTests");
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", getTests);
+
+const port = process.env.PORT || 3003;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// var url = "http://localhost:9090/?wsdl";
+// var args = { name: "value" };
+
+// soap.createClient(url, function(err, client) {
+//     client.GetInfo(args, function(err, result) {
+//         console.log(result);
+//     });
+// });
---
+++ routes/getTests.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const router = express.Router();
+var soap = require("soap");
+
+const url = "http://localhost:9090/?wsdl";
+var args = { name: "value" };
+
+router.get("/UnitTests", async (req, res) => {
+    const client = await soap.createClientAsync(url);
+    client.UnitTestResultsAll(args, function(err, result) {
+        if (result.UnitTestResultsAllResult) {
+            let testResult = result.UnitTestResultsAllResult.string.map(
+                item => {
+                    return JSON.parse(item);
+                }
+            );
+            res.send(testResult);
+        } else {
+            return res.status(404).send("No data found");
+        }
+    });
+});
+
+router.get("/UnitTests/GetNonPassingTest", async (req, res) => {
+    const client = await soap.createClientAsync(url);
+    client.GetNonPassingTest(args, function(err, result) {
+        if (result.GetNonPassingTestResult) {
+            let testResult = result.GetNonPassingTestResult.string.map(item => {
+                return JSON.parse(item);
+            });
+            res.send(testResult);
+        } else {
+            return res.status(404).send("No data found");
+        }
+    });
+});
+
+router.get("/UnitTests/GetPassingTest", async (req, res) => {
+    const client = await soap.createClientAsync(url);
+    client.GetPassingTest(args, function(err, result) {
+        if (result.GetPassingTestResult) {
+            let testResult = result.GetPassingTestResult.string.map(item => {
+                return JSON.parse(item);
+            });
+            res.send(testResult);
+        } else {
+            return res.status(404).send("No data found");
+        }
+    });
+});
+
+router.get("/UnitTests/GetFailedTest", async (req, res) => {
+    const client = await soap.createClientAsync(url);
+    client.GetFailedTest(args, function(err, result) {
+        if (result.GetFailedTestResult) {
+            let testResult = result.GetFailedTestResult.string.map(item => {
+                return JSON.parse(item);
+            });
+            res.send(testResult);
+        } else {
+            return res.status(404).send("No data found");
+        }
+    });
+});
+
+router.get("/UnitTests/GetInfo", async (req, res) => {
+    console.log(req.query);
+    const client = await soap.createClientAsync(url);
+    client.GetInfo(
+        { testName: req.query.testName, filterType: req.query.filterType },
+        function(err, result) {
+            if (result.GetInfoResult) {
+                if (Array.isArray(result.GetInfoResult.string)) {
+                    let testResult = result.GetInfoResult.string.map(item => {
+                        return JSON.parse(item);
+                    });
+                    res.send(testResult);
+                } else {
+                    let testResult = new Array();
+                    testResult.push(JSON.parse(result.GetInfoResult.string));
+                    res.send(testResult);
+                }
+            } else {
+                return res.status(404).send("No data found");
+            }
+        }
+    );
+});
+
+module.exports = router;
